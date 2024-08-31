8dec528888859fcd76d945a64352c892edfe283c
tszhong0411 <me@honghong.me>
2024-08-31 23:17:18 +0800

    feat: square

--- struct_rectangle_area/src/main.rs
+++ struct_rectangle_area/src/main.rs
@@ -8,18 +8,39 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
+    let square = Rectangle::square(3);
+
     let rectangle = Rectangle {
         width: 30,
         height: 50,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rectangle.area()
-    );
+    let rectangle2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rectangle3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("{}", rectangle.can_hold(&rectangle2));
+    println!("{}", rectangle2.can_hold(&rectangle3));
 
     println!("{:#?}", rectangle);
 }
