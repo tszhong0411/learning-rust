4d40a0e6b9d977c444b5a5b37f44dd5b7bb82d5d
tszhong0411 <me@honghong.me>
2024-08-31 23:00:42 +0800

    feat: struct

---
+++ struct_demo/src/main.rs
@@ -0,0 +1,42 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 512,
+    };
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        ..user1
+    };
+
+    // tuple struct
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    // unit-like struct
+    struct UnitLikeStruct;
+}
